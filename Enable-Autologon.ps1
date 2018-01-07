beced7ce43f0720df7566e547d8fff3239fd668f
Anthony Fontanez <ajfrcc@gmail.com>
2018-01-07 17:03:35 -0500

    Adding Autologon scripts

---
+++ Enable-Autologon.ps1
@@ -0,0 +1,29 @@
+<#
+    .Synopsis
+        This script will retrive autologon credentials from ConfigMgr Task Sequence variables and set them using Sysinternals Autologon.exe.
+    .Example
+        ./Enable-Autologon.ps1
+    .Description
+        This script will retrive autologon credentials from ConfigMgr Task Sequence variables and set them using Sysinternals Autologon.exe.
+        It requires Autologon.exe within the same content location as this script.
+    .Notes
+        NAME: Enable-Autologon.ps1
+        AUTHOR: Anthony Fontanez (ajfrcc@gmail.com)
+        VERSION: 1.0
+        LASTEDIT: 2018-01-07
+        CHANGELOG:
+            1.0 (2018-01-07) Initial script creation
+#>
+
+$TSEnv = New-Object -COMObject Microsoft.SMS.TSEnvironment
+$AutologonUsername = $TSEnv.Value("AutologonUsername")
+$AutologonPassword = $TSEnv.Value("AutologonPassword")
+$AutologonDomain  = $TSEnv.Value("AutologonDomain")
+$RegistryPath = "HKCU:\Software\Sysinternals\Autologon"
+$Name = "EulaAccepted"
+$Value = "1"
+$ScriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+New-Item -Path $RegistryPath -Force
+New-ItemProperty -Path $RegistryPath -Name $Name -Value $Value -PropertyType DWORD -Force
+Start-Process -FilePath "$($ScriptDirectory)\Autologon.exe" -ArgumentList "$AutologonUsername $AutologonDomain $AutologonPassword"
