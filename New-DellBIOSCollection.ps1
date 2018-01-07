82d25dcfc81f533d647093a4f2e948eefed75dc2
Anthony Fontanez <ajfrcc@gmail.com>
2018-01-07 16:38:41 -0500

    Initial commit of Dell BIOS app and collection scripts for creating BIOS apps and deploying them

---
+++ New-DellBIOSCollection.ps1
@@ -0,0 +1,19 @@
+﻿Import-Module "$($env:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1"
+
+$SiteCode = "SMS"
+$IncludeCollection = "Clients Eligible for BIOS Updates"
+$Models = @(
+	"OptiPlex 9010",
+	"OptiPlex 9020",
+	"OptiPlex 7040"
+)
+
+Set-Location "$($SiteCode):"
+
+$Schedule = New-CMSchedule -RecurInterval Days -RecurCount 1
+
+foreach ($Model in $Models) {
+    New-CMDeviceCollection -Name "BIOS Update - $Model" -LimitingCollectionName $Model -RefreshSchedule $Schedule -RefreshType Periodic
+    Get-CMDeviceCollection -Name "BIOS Update - $Model" | Move-CMObject -FolderPath "$($SiteCode):\DeviceCollection\BIOS"
+    Add-CMDeviceCollectionIncludeMembershipRule -CollectionName "BIOS Update - $Model" -IncludeCollectionName $IncludeCollection
+}
