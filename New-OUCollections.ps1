7301378a855e7e388bf21299776b2ee4f981f3eb
Anthony Fontanez <ajf@anthonyfontanez.com>
2021-02-05 15:30:11 -0500

    Adding New-OUCollections.ps1

---
+++ New-OUCollections.ps1
@@ -0,0 +1,43 @@
+[CmdletBinding()]
+Param(
+	[Parameter(Mandatory=$true)]
+	[string]$SiteCode,
+	[Parameter(Mandatory=$true)]
+	[string]$Folder,
+	[Parameter(Mandatory=$true)]
+	[string]$DomainName,
+	[Parameter(Mandatory=$true)]
+	[string]$RootOUName
+)
+
+Import-Module -Name ActiveDirectory
+Import-Module -FullyQualifiedName "$env:SMS_ADMIN_UI_PATH\..\..\bin\ConfigurationManager.psd1"
+
+$FolderPath = "${SiteCode}:\DeviceCollection\$($Folder)"
+$DomainDN   = Get-ADDomain -Identity $DomainName | Select-Object -ExpandProperty DistinguishedName
+$OUs        = Get-ADOrganizationalUnit -Filter * -SearchBase "OU=$($RootOUName),$($DomainDN)" -Properties CanonicalName,DistinguishedName,Name,Description | Select-Object -Property CanonicalName,DistinguishedName,Name,Description | Sort-Object -Property CanonicalName
+$AllOUs     = Get-ADOrganizationalUnit -Filter * -Properties CanonicalName
+
+Set-Location -Path "$($SiteCode):"
+
+$RefreshInterval = New-CMSchedule -RecurCount 1 -RecurInterval Days
+
+foreach ($OU in $OUs) {
+	$Name        = $OU.Name
+	$Description = $OU.Description
+	$DN          = $OU.DistinguishedName
+	$CN          = $OU.CanonicalName
+	$ParentDN    = (([adsi]"LDAP://$($OU.DistinguishedName)").Parent).Substring(7)
+	$Parent      = $AllOrgUnits | Where-Object -FilterScript {$_.DistinguishedName -like $ParentDN} | Select-Object -Property CanonicalName,Name
+	$ParentCN    = $Parent.CanonicalName
+	$ParentName  = $Parent.Name
+	$Comment     = "$Name | $Description | ""$DN"""
+	
+	New-CMDeviceCollection -LimitingCollectionName "All Systems" -Name $CN -RefreshSchedule $RefreshInterval -Comment $Comment | Out-Null
+	Add-CMDeviceCollectionQueryMembershipRule -CollectionName $CN -QueryExpression "select * from SMS_R_System where SMS_R_System.SystemOUName = '$CN'" -RuleName $CN
+	Move-CMObject -FolderPath $FolderPath -ObjectId (Get-CMDeviceCollection -Name $CN).CollectionID
+	
+	Write-Host -Object "Device Collection ""$CN"" created"
+}
+
+Set-Location -Path $env:SystemDrive
